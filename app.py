c73318e0dc83324cc6c6ba8019c1c64725100fd7
Neemat Okyere <okyereneemat27@gmail.com>
2026-08-16 03:24:44 +0000

    Prepare app for deployment

--- app.py
+++ app.py
@@ -74,5 +74,8 @@ def report():
     """
     return html
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
